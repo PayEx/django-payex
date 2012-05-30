58a4373bf6d6c354cf29949ba2d33cfccf8ea1e2
Jon Lønne <jon@funkbit.no>
2012-05-30 18:15:56 +0200

    Added Django admin config for Agreement and AutoPayStatus models.

--- djpayex/admin.py
+++ djpayex/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 
-from djpayex.models import InitializedPayment, TransactionStatus
+from djpayex.models import InitializedPayment, TransactionStatus, Agreement, AutoPayStatus
 
 
 class InitializedPaymentAdmin(admin.ModelAdmin):
@@ -59,3 +59,52 @@ class TransactionStatusAdmin(admin.ModelAdmin):
     #readonly_fields = TransactionStatus._meta.get_all_field_names()
 
 admin.site.register(TransactionStatus, TransactionStatusAdmin)
+
+class AgreementAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (_('Response status'), {
+            'fields': ('errorcode', 'description', 'paramname', 'thirdpartyerror', )
+        }),
+        (_('Agreement'), {
+            'fields': ('agreementref', 'maxamount', )
+        }),
+        (_('Timestamps'), {
+            'fields': () # 'created', 'updated', 
+        }),
+        (_('Raw response'), {
+            'classes': ('collapse',),
+            'fields': ('raw_response', )
+        }),
+    )
+    list_display = ('__unicode__', 'agreementref', 'created', )
+    list_filter = ('created', )
+    search_fields = ('id', 'agreementref', )
+    #readonly_fields = Agreement._meta.get_all_field_names()
+
+admin.site.register(Agreement, AgreementAdmin)
+
+class AutoPayStatusAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (_('Response status'), {
+            'fields': ('errorcode', 'description', 'paramname', 'thirdpartyerror', )
+        }),
+        (_('Transaction'), {
+            'fields': ('transactionstatus', 'transactionnumber', 'transactionref', )
+        }),
+        (_('Payment information'), {
+            'fields': ('paymentmethod', )
+        }),
+        (_('Timestamps'), {
+            'fields': () # 'created', 'updated', 
+        }),
+        (_('Raw response'), {
+            'classes': ('collapse',),
+            'fields': ('raw_response', )
+        }),
+    )
+    list_display = ('__unicode__', 'transactionnumber', 'created', )
+    list_filter = ('created', )
+    search_fields = ('id', 'transactionnumber', )
+    #readonly_fields = AutoPayStatus._meta.get_all_field_names()
+
+admin.site.register(AutoPayStatus, AutoPayStatusAdmin)
