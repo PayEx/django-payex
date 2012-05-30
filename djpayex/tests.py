6552827ea8c141165e92f99d5e14611d7788f143
Jon Lønne <jon@funkbit.no>
2012-05-30 11:47:18 +0200

    Various enhancements to the tests.

--- djpayex/tests.py
+++ djpayex/tests.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 
@@ -12,7 +13,23 @@ class InitializedPaymentTests(TestCase):
         Test manager method for an initiated payment.
         """
         
-        response = {'status': {'errorCode': 'OK', 'code': 'OK', 'description': 'OK', 'thirdPartyError': None, 'paramName': None}, 'header': {'date': '2011-10-07 12:35:34', 'name': 'Payex Header v1.0', 'id': 'a3a2da2c9cdd4aebbcaa6b609df6fd52'}, 'sessionRef': '5996a5a093af4820a8c5755a73e6e76b', 'redirectUrl': 'https://test-account.payex.com/MiscUI/PxMenu.aspx?orderRef=c59cc7c7cc0c4194b1e21c27d6ba4074', 'orderRef': 'c59cc7c7cc0c4194b1e21c27d6ba4074'}
+        response = {
+            'status': {
+                'errorCode': 'OK', 
+                'code': 'OK', 
+                'description': 'OK', 
+                'thirdPartyError': None, 
+                'paramName': None
+            }, 
+            'header': {
+                'date': '2011-10-07 12:35:34', 
+                'name': 'Payex Header v1.0', 
+                'id': 'a3a2da2c9cdd4aebbcaa6b609df6fd52'
+            }, 
+            'sessionRef': '5996a5a093af4820a8c5755a73e6e76b', 
+            'redirectUrl': 'https://test-account.payex.com/MiscUI/PxMenu.aspx?orderRef=c59cc7c7cc0c4194b1e21c27d6ba4074', 
+            'orderRef': 'c59cc7c7cc0c4194b1e21c27d6ba4074'
+        }
         
         # Don't save
         obj = InitializedPayment.objects.create_from_response(response, commit=False)
@@ -39,7 +56,21 @@ class TransactionStatusTests(TestCase):
         Test manager create method for an unfinished transaction.
         """
         
-        response = {'status': {'errorCode': 'Order_OrderProcessing', 'code': 'Order_OrderProcessing', 'description': 'The user has not initialized the purchase yet', 'thirdPartyError': None, 'paramName': None}, 'header': {'date': '2011-10-07 12:53:53', 'name': 'Payex Header v1.0', 'id': 'd9d4c533c8484a7c8141fda341e830e1'}, 'orderStatus': '1'}
+        response = {
+            'status': {
+                'errorCode': 'Order_OrderProcessing', 
+                'code': 'Order_OrderProcessing', 
+                'description': 'The user has not initialized the purchase yet', 
+                'thirdPartyError': None, 
+                'paramName': None
+            },
+            'header': {
+                'date': '2011-10-07 12:53:53', 
+                'name': 'Payex Header v1.0', 
+                'id': 'd9d4c533c8484a7c8141fda341e830e1'
+            },
+            'orderStatus': '1'
+        }
         
         # Don't save
         obj = TransactionStatus.objects.create_from_response(response, commit=False)
@@ -64,7 +95,38 @@ class TransactionStatusTests(TestCase):
         Test manager create method for a completed transaction.
         """
         
-        response = {'status': {'errorCode': 'OK', 'code': 'OK', 'description': 'OK', 'thirdPartyError': None, 'paramName': None}, 'orderId': 'test1', 'transactionNumber': '40276785', 'clientAccount': '0', 'pending': False, 'alreadyCompleted': False, 'clientGsmNumber': None, 'productNumber': '123', 'AuthenticatedStatus': '3DSecure', 'maskedNumber': '41**********1111', 'header': {'date': '2011-10-07 12:59:30', 'name': 'Payex Header v1.0', 'id': 'efb64f26a27449b9bd513f949a077080'}, 'amount': '5000', 'fraudData': False, 'orderStatus': '0', 'AuthenticatedWith': 'Y', 'transactionRef': 'e4ee430eba5a4cdb85f7e81a93c2e424', 'transactionStatus': '0', 'paymentMethod': 'VISA', 'BankHash': '12300001-4111-1111-1111-000000000000', 'productId': '123'}
+        response = {
+            'status': {
+                'errorCode': 'OK', 
+                'code': 'OK', 
+                'description': 'OK', 
+                'thirdPartyError': None, 
+                'paramName': None
+            }, 
+            'orderId': 'test1', 
+            'transactionNumber': '40276785', 
+            'clientAccount': '0', 
+            'pending': False, 
+            'alreadyCompleted': False, 
+            'clientGsmNumber': None, 
+            'productNumber': '123', 
+            'AuthenticatedStatus': '3DSecure', 
+            'maskedNumber': '41**********1111', 
+            'header': {
+                'date': '2011-10-07 12:59:30', 
+                'name': 'Payex Header v1.0', 
+                'id': 'efb64f26a27449b9bd513f949a077080'
+            }, 
+            'amount': '5000', 
+            'fraudData': False, 
+            'orderStatus': '0', 
+            'AuthenticatedWith': 'Y', 
+            'transactionRef': 'e4ee430eba5a4cdb85f7e81a93c2e424', 
+            'transactionStatus': '0', 
+            'paymentMethod': 'VISA', 
+            'BankHash': '12300001-4111-1111-1111-000000000000', 
+            'productId': '123'
+        }
         
         # Don't save
         obj = TransactionStatus.objects.create_from_response(response, commit=False)
@@ -91,7 +153,7 @@ class TransactionStatusTests(TestCase):
         self.assertFalse(obj.alreadycompleted)
         self.assertFalse(obj.frauddata)
         
-        self.assertTrue(obj.is_completed_successfully)
+        self.assertTrue(obj.is_completed_successfully())
 
 class CallbackTests(TestCase):
     
@@ -104,6 +166,24 @@ class CallbackTests(TestCase):
         response = self.client.get(reverse('payex-callback'))
         self.assertTrue(response.status_code, 405)
         
-        # POST a callback
+        # POST a callback with missing argument
         response = self.client.post(reverse('payex-callback'), {})
         self.assertTrue(response.status_code, 200)
+        self.assertEquals(response.content, 'FAILURE')
+        
+        # Only test against PayEx test server
+        if not getattr(settings, 'PAYEX_IN_PRODUCTION', True):
+            
+            # POST a valid callback 
+            response = self.client.post(reverse('payex-callback'), {
+                'transactionRef': '123',
+                'transactionNumber': '456',
+                'orderRef': 'abc123',
+            })
+            self.assertTrue(response.status_code, 200)
+            self.assertEquals(response.content, 'OK')
+            
+            # Check object
+            self.assertEquals(TransactionStatus.objects.count(), 1)
+            
+            status = TransactionStatus.objects.get()
--- setup.py
+++ setup.py
@@ -5,24 +5,23 @@ import sys
 from djpayex import __version__
 from setuptools import setup
 
-def publish():
-    """Publish to Pypi"""
-    os.system("python setup.py sdist upload")
 
+# Publish to Pypi
 if sys.argv[-1] == "publish":
-    publish()
+    os.system("python setup.py sdist upload")
     sys.exit()
 
-setup(name='django-payex',
-      version=__version__,
-      description='Django application for saving statuses and receiving callbacks from the PayEx API.',
-      long_description=open('README.md').read(),
-      author='Funkbit AS',
-      author_email='post@funkbit.no',
-      url='https://github.com/funkbit/django-payex',
-      packages=['djpayex',],
-      license='BSD',
-      classifiers = (
+setup(
+    name='django-payex',
+    version=__version__,
+    description='Django application for saving statuses and receiving callbacks from the PayEx API.',
+    long_description=open('README.md').read(),
+    author='Funkbit AS',
+    author_email='post@funkbit.no',
+    url='https://github.com/funkbit/django-payex',
+    packages=['djpayex',],
+    license='BSD',
+    classifiers = (
         "Development Status :: 3 - Alpha",
         "Framework :: Django",
         "Intended Audience :: Developers",
@@ -30,5 +29,5 @@ setup(name='django-payex',
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        )
-     )
+    )
+)
